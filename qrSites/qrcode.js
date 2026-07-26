650eadf0c356409cc7544e0a0a25deae33398e21
luiscript-ed <duduzinho310310@gmail.com>
2026-07-26 00:11:57 -0300

    Atualização geral, adicionei um monte de coisa, com a conexão no Backend de LocalStorage

---
+++ qrSites/qrcode.js
@@ -0,0 +1,117 @@
+// Tenta buscar o valor Se não existir dá 0.
+let pontoBanco = localStorage.getItem("pontos") || "0";
+let qrBanco = localStorage.getItem("qrlidos") || "0";
+
+let listaLocalidades = JSON.parse(localStorage.getItem("locais")) || [];
+
+const parametros = new URLSearchParams(window.location.search);
+const local = parametros.get("local");
+
+const hoje = new Date();
+
+const registro = listaLocalidades.find(item => item.local === local);
+// Tabela de dados com pontos de cada local
+
+
+const locais = {
+
+    Limpec:{
+        nome:"Limpec",
+        pontos:20
+    },
+
+    Shopping:{
+        nome:"Shopping Boulevard",
+        pontos:35
+    },
+
+    Escola:{
+        nome:"Escola Municipal",
+        pontos:15
+    }
+
+};
+
+const recompensa = locais[local];
+
+if (!recompensa) {
+    mensagemH1.innerHTML = "QR Code inválido";
+    mensagemH3.innerHTML = "Este QR Code não é reconhecido.";
+
+    throw new Error("QR inválido");
+}
+
+let dataValida = false
+
+//Começo do Fim
+
+const mensagemH1 = document.getElementById("mensagemH1");
+const mensagemH3 = document.getElementById("mensagemH3");
+
+function verifydate(){
+    if(!registro){
+        dataValida = true;
+        return;
+    }
+    const dataAntiga = new Date(registro.data);
+    const diferenca = hoje - dataAntiga;
+    const dias = diferenca / (1000*60*60*24);
+
+    dataValida = dias >= 30;
+
+}
+
+verifydate()
+
+if( registro && dataValida == false){
+    mensagemH1.innerHTML = "Oops"
+    mensagemH3.innerHTML = "Voce já resgatou esse local. O certo é só resgatar uma vez por mês!!"
+} else {
+    if (registro) {
+
+        registro.data = hoje.toISOString().split("T")[0];
+
+    }else{
+
+        listaLocalidades.push({
+            local: local,
+            data: hoje.toISOString().split("T")[0]
+        });
+
+    }
+
+        
+        localStorage.setItem("ultimaAtividade", JSON.stringify({
+
+        local: local,
+
+        pontos: recompensa.pontos,
+
+        data: hoje.toLocaleDateString()
+
+    }));
+
+   // Converte o texto para número e soma
+    let somaPonto = parseInt(pontoBanco) + recompensa.pontos;
+    let somaQr = parseInt(qrBanco) + 1;
+
+    // Salva o resultado atualizado de volta
+    localStorage.setItem("pontos", somaPonto);
+    localStorage.setItem("qrlidos", somaQr);
+    localStorage.setItem("locais", JSON.stringify(listaLocalidades));
+
+    mensagemH1.innerHTML = "Parabéns";
+    mensagemH3.innerHTML =
+`Você acabou de ganhar ${recompensa.pontos} pontos e ficou com ${somaPonto} pontos no total!`;
+    setTimeout(()=>{
+
+        window.location.href="https://luiscript-ed.github.io/ReciclaMais/site-map/mapa.html";
+
+    },3000);
+}
+
+
+
+
+
+
---
+++ site-map/pontos.js
@@ -0,0 +1,88 @@
+let ponto = parseInt(localStorage.getItem("pontos")) || 0;
+let qrLidos = localStorage.getItem('qrlidos');
+
+const pontoFront = document.getElementById("pontosUsuario");
+const medalhaUsuario = document.getElementById("medalhaUsuario");
+const meta = document.getElementById("meta");
+
+let metaValor = 0;
+const qr = document.getElementById("qrLidos");
+
+const medalha3 = document.querySelector(".medalha3");
+const medalha2 = document.querySelector(".medalha2");
+const medalha1 = document.querySelector(".medalha1");
+const medalhaDima = document.querySelector(".medalhaDima");
+
+let porcentagem = 0;
+
+if (ponto <= 150) {
+    medalhaUsuario.innerHTML = "Aprendiz natural";
+    medalha3.classList.add("desbloqueada");
+    metaValor = 150
+    meta.innerHTML = "150";
+
+} else if (ponto > 150 && ponto <= 500){
+    medalhaUsuario.innerHTML = "Guardião Verde";
+    meta.innerHTML = "500";
+    metaValor = 500
+    medalha2.classList.add("desbloqueada");
+
+} else if (ponto > 500 && ponto <= 2000) {
+    medalhaUsuario.innerHTML = "Monge da sustentabilidade";
+    meta.innerHTML = "2000";
+    metaValor = 2000;
+    medalha1.classList.add("desbloqueada");
+
+} else if (ponto > 2000 ) {
+    medalhaUsuario.innerHTML = "Mestre RECICLA+";
+    meta.innerHTML = "ACABOU AS METAS 😲 🎉";
+    medalhaDima.classList.add("desbloqueada");
+
+}
+
+qr.innerHTML = qrLidos;
+pontoFront.innerHTML = ponto;
+
+//Aqui fica a parte da progressão
+
+const divisaoSpan = document.getElementById("divisao");
+const porcSpan = document.getElementById("porcentagem");
+
+let porcen = (ponto/metaValor)*100;
+divisaoSpan.innerHTML = `${ponto} / ${metaValor}`;
+porcSpan.innerHTML = `${porcen.toFixed(1)}%`;
+
+const barra = document.querySelector(".barraInterna");
+
+barra.style.width = `${porcen}%`;
+
+// Criando fisicamente a div do card antes de usar
+    const card = document.createElement("div");
+     card.className = "historico";
+
+     const ultima = JSON.parse(localStorage.getItem("ultimaAtividade"));
+    // Montando o HTML interno do card 
+    card.innerHTML = `
+
+        <h3>Última atividade</h3>
+
+            <ul>
+
+                <li>
+
+                    📍 ${ultima.local}
+
+                    <br>
+
+                        ⭐ +${ultima.pontos} pontos
+
+                    <br>
+
+                        📅 ${ultima.data}
+
+                    </li>
+
+                </ul>
+
+`;
+            containerHistorico.appendChild(card);
