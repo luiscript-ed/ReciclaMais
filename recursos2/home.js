2a9b7c8c2f7747c5591dca519cd3d8ec3cc530c9
luiscript-ed <duduzinho310310@gmail.com>
2026-07-27 17:41:19 -0300

    Adicionando detalhes finais

--- recursos2/home.js
+++ recursos2/home.js
@@ -13,8 +13,13 @@ document.getElementById("username").textContent =
     window.location.href="https://luiscript-ed.github.io/ReciclaMais/site-map/mapa.html";
   })
 
+  const buttonMap = document.getElementById("mapaLink")
+
+    buttonMap.addEventListener("click", function(){
+    window.location.href="https://luiscript-ed.github.io/ReciclaMais/site-map/mapa.html";
+  })
    const buttonForms = document.getElementById("form");
 
-   FormData.addEventListener("click", function(){
+   buttonForms.addEventListener("click", function(){
     window.location.href="https://docs.google.com/forms/d/e/1FAIpQLScAx-F0cmU5muKfCcf9tGJcKMEn1fcJXL_3cjPBr0q6bbxZ-g/viewform?usp=dialog";
   })
